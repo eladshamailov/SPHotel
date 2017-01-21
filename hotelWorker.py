a3da67ec41cd14fc91925e67bd64903bb3b20a8d
elad <eladsham@post.bgu.ac.il>
2017-01-21 17:23:13 +0200

    start

--- hotelWorker.py
+++ hotelWorker.py
@@ -1,11 +1,12 @@
 import os
 import sqlite3
 
-def dohoteltask(taskname,parameter)
+def dohoteltask(taskname,parameter):
     if os.path.isfile('cronhoteldb.db'):
         dbcon = sqlite3.connect('cronhoteldb.db')
         with dbcon:
             cursor = dbcon.cursor()
             if taskname=="breakfast":
-                fullName=cursor.execute("SELECT FirstName,LastName FROM Residents WHERE RoomNumber =
+                cursor.execute("SELECT FirstName,LastName FROM Residents WHERE RoomNumber = (?)",(parameter,))
+                fullName=cursor.fetchone()
 
