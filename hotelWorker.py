82b27f15df07c39d4c308d2130e55408ef22de3b
elad <eladsham@post.bgu.ac.il>
2017-01-21 17:15:35 +0200

    updating worker

--- hotelWorker.py
+++ hotelWorker.py
@@ -1,2 +1,10 @@
 import os
 import sqlite3
+
+def dohoteltask(taskname,parameter)
+    if os.path.isfile('cronhoteldb.db'):
+        dbcon = sqlite3.connect('cronhoteldb.db')
+        with dbcon:
+            cursor = dbcon.cursor()
+            if taskname=="breakfast":
+                fullName=cursor.execute(SELECT FirstName,LastName FROM Residents WHERE )
