e3d48c76c7903f7083ba6a16f034934bde9f46d8
elad <eladsham@post.bgu.ac.il>
2017-01-21 18:11:10 +0200

    start

--- hotelWorker.py
+++ hotelWorker.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import time
 
 def dohoteltask(taskname,parameter):
     if os.path.isfile('cronhoteldb.db'):
@@ -9,4 +10,17 @@ def dohoteltask(taskname,parameter):
             if taskname=="breakfast":
                 cursor.execute("SELECT FirstName,LastName FROM Residents WHERE RoomNumber = (?)", (parameter,))
                 fullName=cursor.fetchone()
-
+                print (fullName[0]+" "+fullName[1]+" in room "+parameter+" has been served breakfast at "+str(time.time()))
+                ans=time.time()
+                cursor.execute("UPDATE TaskTimes SET NumTimes=(?) WHERE TasksTimes.TaskId=(SELECT TaskId FROM Tasks WHERE Tasks.parameter=(?) AND Tasks.TaskName=(?))", [NumTimes-1, parameter, 'breakfast'])
+                return ans
+            else:
+                if taskname=="wakeup":
+                    cursor.execute("SELECT FirstName,LastName FROM Residents WHERE RoomNumber = (?)", (parameter,))
+                    fullName = cursor.fetchone()
+                    print (fullName[0]+" "+fullName[1]+" in room "+parameter+" has been served breakfast at "+str(time.time()))
+                    ans = time.time()
+                    cursor.execute("UPDATE TaskTimes SET NumTimes=(?) WHERE TasksTimes.TaskId=(SELECT TaskId FROM Tasks WHERE Tasks.parameter=(?) AND Tasks.TaskName=(?))", [NumTimes-1, parameter, 'wakeup'])
+                    return ans
+                else:
+                    if taskname=="clean":
